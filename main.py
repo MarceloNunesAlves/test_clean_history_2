2119eca3474e78490c533d80589dfb38b59bc9b0
Marcelo Nunes Alves <marcelo.nunes@nava.com.br>
2025-02-14 09:24:54 -0300

    Remocão das chaves arquivo python

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from dotenv import load_dotenv
 import os
 
-senha = os.environ.get('PWD_DB', "<SECRET>")
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv(f".env")
+
+senha = os.environ.get('PWD_DB')
 print(f"{senha}")
 
-senha = os.environ.get('API_KEY', "<SECRET>")
+senha = os.environ.get('API_KEY')
 print(f"{senha}")
 
-senha = os.environ.get('AWS_ACCESS_KEY', "354646874")
+senha = os.environ.get('AWS_ACCESS_KEY')
 print(f"{senha}")
 
-senha = os.environ.get('PWD_POST', "<SECRET>")
+senha = os.environ.get('PWD_POST')
 print(f"{senha}")
